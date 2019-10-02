705563ffcd861f7e89d351e886326c4e7e4c76ed
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-10-02 23:37:07 +0700

    Create router for /login.

--- api/auth/index.js
+++ api/auth/index.js
@@ -8,11 +8,21 @@ const {
 } = require('../../helpers/responsers')
 
 const {
-  registerUser
+  registerUser,
+  login
 } = require('./controllers')
 
 router.post('/register', function(req, res) {
   Promise.try(() => registerUser(req.body))
+    .then(response => res.status(response.statusCode).json(response))
+    .catch(error => {
+      console.log(error)
+      res.status(500).send(server_error_internal(`App Server Error, please contact the admin.`))
+    })
+})
+
+router.post('/login', function(req, res) {
+  Promise.try(() => login(req.body))
     .then(response => res.status(response.statusCode).json(response))
     .catch(error => {
       console.log(error)
